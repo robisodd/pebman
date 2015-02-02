c2052d7e54b81983e0614d4d6ea8ab0fa8607787
robisodd <robisodd@gmail.com>
2015-02-02 11:46:41 -0500

    Eatin' dots!

--- src/main.c
+++ src/main.c
@@ -1,4 +1,5 @@
 //56b37c6f-792a-480f-b962-9a0db8c32aa4
+//b00bface-effd-480f-b962-c0ded4c0ffee
 /************************************************
   Notes
   ************************************************
@@ -56,30 +57,51 @@ Version 2: (Level Editor)
   
 */
 #include "pebble.h"
-#define UPDATE_MS 200 // Refresh rate in milliseconds
+#define UPDATE_MS 30 // Refresh rate in milliseconds
 #define ZOOM 5       // Number of pixels per map square
 #define MAP_W 28
 #define MAP_H 31
-
-struct PlayerStruct {
-	uint16_t x, y; // in pixels, not tile -- center pixel
-	//uint16_t y;
-  uint8_t face;  // 0=Left, 1=Up, 2=Right, 3=Down
-  uint8_t mouth; // 0=Closed, 1=Open, 2=Wide, 3=Open
-  uint8_t speed; 
+#define BOARD_X 2
+#define BOARD_Y 13
+//Cardinal Directions = facing >> 15... maybe.
+#define East 0  // +x
+#define South 1 // +y
+#define West 2  // -x
+#define North 3 // -y
+  
+struct XYStruct {
+  int32_t x;
+  int32_t y;
 };
 
-struct PlayerStruct player;
+struct PlayerStruct {
+// 	uint16_t x, y; // in pixels, not tile -- center pixel
+  struct XYStruct speed;
+  struct XYStruct pos;
+  //uint8_t face;  // 0=Left, 1=Up, 2=Right, 3=Down
+  int16_t facing;             // Player Direction Facing (from 0 - TRIG_MAX_ANGLE)
+  uint8_t frame; // Animation frame. Mouth: 0=Closed, 1=Open, 2=Wide, 3=Open
+  //uint8_t speed;
+  uint8_t score;
+};
+
+struct PlayerStruct player[1];
 //struct Spectre *spectre[4];
 
 static Window *game_window;
 static Layer *game_layer;
 GBitmap *background;
 static uint16_t totalpellets=0;
-GBitmap *playerspritesheet, *playersprite[9];
+GBitmap *playerspritesheet, *playersprite[4][4];
 //static TextLayer *label_text_layer;
+uint8_t dotflashing,dotflashingtimer=0;
+uint8_t speed;
+uint8_t currentplayer=0;
 
-
+  static bool up_button_depressed = false; // Whether Pebble's   Up   button is held
+  static bool dn_button_depressed = false; // Whether Pebble's  Down  button is held
+  static bool sl_button_depressed = false; // Whether Pebble's Select button is held
+//static bool bk_button_depressed = false; // Whether Pebble's  Back  button is held
 
 static int8_t map[MAP_W * MAP_H];  // int8 means cells can be from -127 to 128
 
@@ -134,52 +156,208 @@ static int8_t level[MAP_W * MAP_H] =
 static int32_t abs32(int32_t x) {return (x ^ (x >> 31)) - (x >> 31);}
 
 // ------------------------------------------------------------------------ //
-//  Timer Functions
-// ------------------------------------------------------------------------ //
-static void timer_callback(void *data) {
-  AccelData accel = (AccelData) { .x = 0, .y = 0, .z = 0 };
-  accel_service_peek(&accel); // Read accelerometer
-  /*
-  if(abs32(accel.x)<abs32(accel.y)){
-    if(accel.y<0) JoyStick = JoyUp;
-    else          JoyStick = JoyDown;
-  } else {
-    if(accel.x<0) JoyStick = JoyLeft;
-    else          JoyStick = JoyRight;
-  }
-  */
-  layer_mark_dirty(game_layer);  // Schedule redraw of screen
-}
-
-// ------------------------------------------------------------------------ //
-//  Game Functions
+//  Init Functions
 // ------------------------------------------------------------------------ //
 static void init_board() {
   for(uint16_t i=0; i<MAP_W*MAP_H; i++) map[i] = level[i];
     
-  totalpellets=0; for(uint16_t i=0; i<MAP_W*MAP_H; i++) if(map[i]>0) totalpellets++;  // Count Pellets (not Power Pellets)
 
   //for(uint16_t y=0, i=0; y<MAP_H; y++){
   //  for(uint16_t x=0; x<(MAP_W/2); x++,i++) map[(y*MAP_W)+x] = level[i];
   //  for(uint16_t x=(MAP_W/2); x<MAP_W; x++, i--) *MAP_H; i++) map[(y*MAP_W)+x] = level[i];
   //}
-  
+  speed=19;
 }
 
 static void init_player() {
-  player.x = 69;
-  player.y = 127;
-  player.face = 0;
-  player.mouth = 1;
-  player.speed = 5;//; 80%
+  player[currentplayer].pos.x = (14<<6);//(1<<6)+32;//1<<6;//(13<<6) + 32; // start halfway between 13&14//69;
+  player[currentplayer].pos.y = (23<<6)+32;//3<<6;//14<<6;//23<<6; // start in middle of 23 //127;
+  player[currentplayer].facing = 2;//1<<14;
+  player[currentplayer].frame = 0;
+  player[currentplayer].speed.x=0;//19;
+  player[currentplayer].speed.y=0;
 }
+
+// ------------------------------------------------------------------------ //
+//  Game Functions
+// ------------------------------------------------------------------------ //
+int8_t getmap(int32_t x, int32_t y) {
+  x>>=6; y>>=6;
+  if(y==14 && (x<0 || x>=MAP_W)) return 0;
+  return (x<0 || x>=MAP_W || y<0 || y>=MAP_H) ? -1 : map[(y * MAP_W) + x];
+}
+
+void setmap(int32_t x, int32_t y, int8_t data) {
+  x>>=6; y>>=6;
+  if(x>=0 && x<MAP_W && y>=0 && y<MAP_H)
+    map[(y * MAP_W) + x]=data;
+}
+
+// ------------------------------------------------------------------------ //
+//  Timer Functions
+// ------------------------------------------------------------------------ //
+static void timer_callback(void *data) {
+  struct XYStruct testspeed, pos;
+  int16_t testfacing, tempspeed;
+  pos.x=player[currentplayer].pos.x;
+  pos.y=player[currentplayer].pos.y;
+  tempspeed=speed;
+  
+  //************************************//
+  // Eat Dots, Get Score, Slow Speed
+  //************************************//
+  if(getmap(player[currentplayer].pos.x, player[currentplayer].pos.y)==1) {  // if eat any sort of dot
+    tempspeed -= 2;
+    player[currentplayer].score += 10;
+    setmap(player[currentplayer].pos.x, player[currentplayer].pos.y, 0);
+  }
+  if(getmap(player[currentplayer].pos.x, player[currentplayer].pos.y)==2) {  // if eat any sort of dot
+    tempspeed -= 6;
+    player[currentplayer].score += 50;
+    setmap(player[currentplayer].pos.x, player[currentplayer].pos.y, 0);
+  }
+
+  
+  /* TODO:
+         Use ARCTAN2 to figure X&Y Joystick by XZ and YZ accelerometer
+         Maybe subtle movement = centered joystick position
+*/
+  AccelData accel = (AccelData) { .x = 0, .y = 0, .z = 0 };
+  accel_service_peek(&accel); // Read accelerometer
+  if(abs32(accel.x)<abs32(accel.y)) {
+    testspeed.x = 0;
+    if(accel.y<0) {testspeed.y = 1; testfacing = 3;} // Down
+    else          {testspeed.y =-1; testfacing = 1;} // Up
+  } else {
+    testspeed.y = 0;
+    if(accel.x<0) {testspeed.x =-1; testfacing = 2;} // Left
+    else          {testspeed.x = 1; testfacing = 0;} // Right
+  }
+
+  if(getmap(pos.x+(64*testspeed.x), pos.y+(64*testspeed.y))>=0) {  // if trying to turn and you can turn
+    player[currentplayer].speed.x=testspeed.x;
+    player[currentplayer].speed.y=testspeed.y;
+    player[currentplayer].facing = testfacing;
+    player[currentplayer].pos.x += (speed*player[currentplayer].speed.x);
+    player[currentplayer].pos.y += (speed*player[currentplayer].speed.y);
+    
+    // Wrap around for tunnel
+    if(player[currentplayer].pos.x<-63) player[currentplayer].pos.x += (32<<6);
+    if(player[currentplayer].pos.x>(32<<6)) player[currentplayer].pos.x -= (32<<6);
+    
+    player[currentplayer].frame++;
+
+    // Center in square (vert or horz, whichever not currently traveling)
+    if(player[currentplayer].speed.x==0) player[currentplayer].pos.x = ((player[currentplayer].pos.x>>6)<<6)+32;
+    if(player[currentplayer].speed.y==0) player[currentplayer].pos.y = ((player[currentplayer].pos.y>>6)<<6)+32;
+
+  } else {  // else can't turn, keep going the same direction
+    if(getmap(player[currentplayer].pos.x+(64*player[currentplayer].speed.x), player[currentplayer].pos.y+(64*player[currentplayer].speed.y))>=0) { // if not running into a wall
+      player[currentplayer].pos.x += (speed*player[currentplayer].speed.x);
+      player[currentplayer].pos.y += (speed*player[currentplayer].speed.y);
+      
+      // Wrap around for tunnel
+      if(player[currentplayer].pos.x<-63) player[currentplayer].pos.x += (32<<6);
+      if(player[currentplayer].pos.x>(32<<6)) player[currentplayer].pos.x -= (32<<6);
+      
+      player[currentplayer].frame++;
+      
+      if(player[currentplayer].speed.x==0) player[currentplayer].pos.x = ((player[currentplayer].pos.x>>6)<<6)+32;
+      if(player[currentplayer].speed.y==0) player[currentplayer].pos.y = ((player[currentplayer].pos.y>>6)<<6)+32;
+    } else {
+      // else ran into wall, center sprite
+
+    }
+  }
+  
+  //TODO: (insert here) tend toward the middle
+  
+  dotflashingtimer++;
+  dotflashing=(dotflashingtimer>>2)&1;
+  layer_mark_dirty(game_layer);  // Schedule redraw of screen
+}
+
+
+
 // ------------------------------------------------------------------------ //
 //  Drawing Functions
 // ------------------------------------------------------------------------ //
+//************************************//
+// Background
+//************************************//
+void draw_background(GBitmap *fb) {
+  //*** BG Version 1
+  //graphics_draw_bitmap_in_rect(ctx, background, GRect(0,13,144,155));
 
-//#define DRAW_PIXEL( framebuffer, x, y, white )  framebuffer[((y*160 + x) / 8)] = ( framebuffer[((y*160 + x) / 8)] & (0xFF ^ (0x01 << (x%8))) ) | (white << (x%8))
+  for (uint16_t i=0; i<BOARD_Y*5; i++) ((uint32_t*)(fb->addr))[i] = 0; // Top 13 rows of black -- equivilant to: graphics_context_set_fill_color(ctx, 0); graphics_fill_rect(ctx, GRect(0,0,144,12), 0, GCornerNone);
+  for (uint16_t i=0,j=BOARD_Y*5; i<155*5; i++,j++) ((uint32_t*)(fb->addr))[j] = ((uint32_t*)background->addr)[i]; // Draw Background Image -- equivilant to: graphics_draw_bitmap_in_rect(ctx, background, GRect(0,13,144,155));
+}
 
+//************************************//
+// Dots
+//************************************//
+void draw_dots(GContext *ctx) {
+  totalpellets=0; 
+  graphics_context_set_stroke_color(ctx, 1);
+  graphics_context_set_fill_color(ctx, dotflashing);
+  for(uint16_t y=0, i=0; y<MAP_H; y++)
+    for(uint16_t x=0; x<MAP_W; x++, i++)
+      if(map[i]==1) {
+        totalpellets++; 
+        graphics_draw_pixel(ctx, GPoint(x*5+BOARD_X+2, y*5+BOARD_Y+2));
+      } else if(map[i]==2) {
+        totalpellets++; 
+        graphics_fill_rect(ctx, GRect(x*5+BOARD_X+1,y*5+BOARD_Y+1,3,3), 0, GCornerNone);
+        graphics_fill_rect(ctx, GRect(x*5+BOARD_X+2,y*5+BOARD_Y,1,5), 0, GCornerNone);
+        graphics_fill_rect(ctx, GRect(x*5+BOARD_X,y*5+BOARD_Y+2,5,1), 0, GCornerNone);
+        //graphics_draw_pixel(ctx, GPoint(x*5+BOARD_X+2, y*5+BOARD_Y+2));
+      }
+}
+
+//************************************//
+// Player
+//************************************//
+void draw_player(GContext *ctx) {
+  int32_t x, y;  // player's center dot on the screen
+  x = ((player[currentplayer].pos.x >> 6) * 5) + (((player[currentplayer].pos.x & 63) * 5) / 64);
+  y = ((player[currentplayer].pos.y >> 6) * 5) + (((player[currentplayer].pos.y & 63) * 5) / 64);
+  graphics_draw_bitmap_in_rect(ctx, playersprite[player[currentplayer].facing][(player[currentplayer].frame>>1)&1], GRect(x+BOARD_X-3, y+BOARD_Y-3, 7, 7));
+
+//   graphics_context_set_stroke_color(ctx, dotflashing);
+//   graphics_draw_pixel(ctx, GPoint(x+BOARD_X, y+BOARD_Y));
+}
+
+//************************************//
+// Spectres
+//************************************//
+void draw_spectres(GBitmap *fb) {
+  
+}
+
+//************************************//
+// Top Bar
+//************************************//
+void draw_top(GContext *ctx) {
+  static char text[40];  //Buffer to hold text
+  graphics_context_set_text_color(ctx, 1);  // Text Color
+  //snprintf(text, sizeof(text), "Score:%d Dots:%d", player[currentplayer].score, totalpellets);  // What text to draw
+  snprintf(text, sizeof(text), "(%ld, %ld) %d", player[currentplayer].pos.x, player[currentplayer].pos.y, getmap(player[currentplayer].pos.x, player[currentplayer].pos.y));  // What text to draw
+  //snprintf(text, sizeof(text), "123456789012345678901234");  // Max for "RESOURCE_ID_GOTHIC_14" and "RESOURCE_ID_GOTHIC_14_BOLD"
+  graphics_draw_text(ctx, text, fonts_get_system_font("RESOURCE_ID_GOTHIC_14_BOLD"), GRect(0, -4, 144, 20), GTextOverflowModeWordWrap, GTextAlignmentLeft, NULL);  //Write Text
+  // draw score (6 digits max -- 7 digits technically possible on level 100)
+}
+
+//************************************//
 static void game_layer_update(Layer *me, GContext *ctx) {
+  GBitmap* framebuffer = graphics_capture_frame_buffer(ctx);
+  if(framebuffer) {
+    draw_background(framebuffer);
+    graphics_release_frame_buffer(ctx, framebuffer);
+  }
+    draw_dots(ctx);
+    draw_player(ctx);
+    draw_top(ctx);
+  
   /*
   static char hello[] = "hello";
   
@@ -191,70 +369,26 @@ static void game_layer_update(Layer *me, GContext *ctx) {
   graphics_context_set_text_color(ctx, 1);  // Text Color
   graphics_draw_text(ctx, text, fonts_get_system_font(FONT_KEY_GOTHIC_14), textframe, GTextOverflowModeWordWrap, GTextAlignmentLeft, NULL);  //Write Text
   */
-
-
-//************************************//
-// Background
-//************************************//
-  //*** BG Version 1
-  //graphics_draw_bitmap_in_rect(ctx, background, GRect(0,13,144,155));
-
-  //*** BG Version 2
-  //GBitmap* layerbitmap = (GBitmap*)ctx;
-  //uint32_t* framebuffer = (uint32_t*)layerbitmap->addr;
-  //for (uint16_t i=0; i<13*5; i++) framebuffer[i] = 0; // Top 13 rows of black
-  //for (uint16_t i=0,j=13*5; i<155*5; i++,j++) framebuffer[j] = background_frame[i]; // Draw Background Image.  5 32bit words per row, 155 rows, starting on row 13
-
-  //*** BG Version 3
-  //uint32_t* framebuffer = (uint32_t*)(((GBitmap*)ctx)->addr);
-  //uint32_t* background_frame = (uint32_t*)background->addr;  
-  //for (uint16_t i=0; i<13*5; i++) framebuffer[i] = 0; // Top 13 rows of black
-  //for (uint16_t i=0,j=13*5; i<155*5; i++,j++) framebuffer[j] = background_frame[i]; // Draw Background Image
-
-  //*** BG Version 4
-  for (uint16_t i=0; i<13*5; i++) ((uint32_t*)(((GBitmap*)ctx)->addr))[i] = 0; // Top 13 rows of black -- equivilant to: graphics_context_set_fill_color(ctx, 0); graphics_fill_rect(ctx, GRect(0,0,144,12), 0, GCornerNone);
-  for (uint16_t i=0,j=13*5; i<155*5; i++,j++) ((uint32_t*)(((GBitmap*)ctx)->addr))[j] = ((uint32_t*)background->addr)[i]; // Draw Background Image -- equivilant to: graphics_draw_bitmap_in_rect(ctx, background, GRect(0,13,144,155));
-
-//************************************//
-// Dots
-//************************************//
-  graphics_context_set_stroke_color(ctx, 1);
-  graphics_context_set_fill_color(ctx, 1);
-  for(uint16_t y=0, i=0; y<MAP_H; y++)
-    for(uint16_t x=0; x<MAP_W; x++, i++)
-      if(map[i]==1)
-        graphics_draw_pixel(ctx, GPoint(x*5+4, y*5+15));
-      else if(map[i]==2) {
-        graphics_fill_rect(ctx, GRect(x*5+3,y*5+14,3,3), 0, GCornerNone);
-        //graphics_draw_pixel(ctx, GPoint(x*5+4, y*5+15));
-      }
-//************************************//
-// Player
-//************************************//
-  graphics_draw_bitmap_in_rect(ctx, playersprite[player.mouth], GRect(player.x, player.y, 7, 7));
-  
-//************************************//
-// Spectres
-//************************************//
-
-//************************************//
-// Top Bar
-//************************************//
-  static char text[40];  //Buffer to hold text
-  graphics_context_set_text_color(ctx, 1);  // Text Color
-  snprintf(text, sizeof(text), "Total: %d    %x", totalpellets, (int)ctx);  // What text to draw
-  //snprintf(text, sizeof(text), "123456789012345678901234");  // Max for "RESOURCE_ID_GOTHIC_14" and "RESOURCE_ID_GOTHIC_14_BOLD"
-  //snprintf(text, sizeof(text), "ox10 000000 HI:000000  o");
-  graphics_draw_text(ctx, text, fonts_get_system_font("RESOURCE_ID_GOTHIC_14_BOLD"), GRect(0, -4, 144, 20), GTextOverflowModeWordWrap, GTextAlignmentLeft, NULL);  //Write Text
-// ************************************ //
-
-  
-
-  // draw score (6 digits max -- 7 digits technically possible on level 100)
-  
   app_timer_register(UPDATE_MS, timer_callback, NULL); // Schedule a callback
 }
   
+// ------------------------------------------------------------------------ //
+//  Button Pushing
+// ------------------------------------------------------------------------ //
+void up_push_in_handler(ClickRecognizerRef recognizer, void *context) {up_button_depressed = true;  player[currentplayer].pos.x+=13;}
+void up_release_handler(ClickRecognizerRef recognizer, void *context) {up_button_depressed = false; }
+void dn_push_in_handler(ClickRecognizerRef recognizer, void *context) {dn_button_depressed = true;  player[currentplayer].pos.x-=13;}
+void dn_release_handler(ClickRecognizerRef recognizer, void *context) {dn_button_depressed = false; }
+void sl_push_in_handler(ClickRecognizerRef recognizer, void *context) {sl_button_depressed = true;  }  // SELECT button was pushed in
+void sl_release_handler(ClickRecognizerRef recognizer, void *context) {sl_button_depressed = false; }
+
+static void click_config_provider(void *context) {
+  window_raw_click_subscribe(BUTTON_ID_UP, up_push_in_handler, up_release_handler, context);
+  window_raw_click_subscribe(BUTTON_ID_DOWN, dn_push_in_handler, dn_release_handler, context);
+  window_raw_click_subscribe(BUTTON_ID_SELECT, sl_push_in_handler, sl_release_handler, context);
+  //window_single_click_subscribe(BUTTON_ID_SELECT, sl_push_in_handler);
+}
+
 // ------------------------------------------------------------------------ //
 //  Main Functions
 // ------------------------------------------------------------------------ //
@@ -264,13 +398,20 @@ static void game_window_load(Window *window) {
   background = gbitmap_create_with_resource(RESOURCE_ID_BACKGROUND);
 // Load game sprites
   playerspritesheet = gbitmap_create_with_resource(RESOURCE_ID_PLAYER_SPRITESHEET);
-  for (uint16_t i=0,j=0; i<9; i++,j+=7) playersprite[i] = gbitmap_create_as_sub_bitmap(playerspritesheet, GRect(j,0,7,7));
+//   for (uint16_t i=0,j=0; i<9; i++,j+=7) playersprite[i] = gbitmap_create_as_sub_bitmap(playerspritesheet, GRect(j,0,7,7));
+  for (uint8_t i=0; i<4; i++)
+    for(uint8_t j=0; j<4;j++)
+      playersprite[i][j] = gbitmap_create_as_sub_bitmap(playerspritesheet, GRect(i*7,j*7,7,7));
+
   init_board();
   init_player();
+  currentplayer=0;
+  player[currentplayer].score=0;
 }
 
 static void game_window_unload(Window *window) {
-  for (uint16_t i=0; i<9; i++) gbitmap_destroy(playersprite[i]);
+//   for (uint16_t i=0; i<9; i++) gbitmap_destroy(playersprite[i]);
+  for (uint8_t i=0; i<4; i++) for (uint8_t j=0; j<4; j++) gbitmap_destroy(playersprite[i][j]);
   gbitmap_destroy(background);
 }
 
@@ -280,6 +421,7 @@ static void game_window_disappear(Window *window) {}
 static void init(void) {
   // Set up and push main window
   game_window = window_create();
+  window_set_click_config_provider(game_window, click_config_provider);
   window_set_window_handlers(game_window, (WindowHandlers) {
     .load = game_window_load,
     .unload = game_window_unload,
